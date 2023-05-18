49b958382f3ef2f4a3d9a798ada7ed448969c79d
Vladislav Sedinkin <sed_flash@mail.ru>
2023-05-18 13:52:59 +0500

    Initial commit

---
+++ Scripts/.setup_instruments.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ETC_DIR=".etc"
+export UTILS_PATH="$ETC_DIR/utils"
+export UTILS_BIN_PATH="$ETC_DIR/bin"
+
+mkdir -p $UTILS_PATH
+mkdir -p $UTILS_BIN_PATH
+
+INSTRUMENTS_LIST_FILE=$1
+
+required_instruments=$(cat $INSTRUMENTS_LIST_FILE | grep -v -E "^#.*")
+
+ORIGINAL_IFS=$IFS
+IFS='
+'
+
+for instrument in $required_instruments; do
+	repository=$(echo $instrument | cut -w -f1)
+	version=$(echo $instrument | cut -w -f2)
+	execulable=$(echo $instrument | cut -w -f3)
+
+	./.install_instruments.sh $repository $version $execulable
+done
+
+IFS=$ORIGINAL_IFS
---
+++ Scripts/bootstrap_instruments.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "🦄 Install instruments... 🦄"
+./.setup_instruments.sh ./.instruments.txt
